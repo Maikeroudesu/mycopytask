90e25e7c8c7c7275f97868e42283e354ac10a169
Maikeroudesu <erizmichael@gmail.com>
2024-09-28 11:28:17 +0800

    ok na

---
+++ tasktoday1.py
@@ -0,0 +1,20 @@
+age= [-12,13,-14,15,-16]
+
+for data in age: 
+    if data < 0:
+      print(str(data) + "negative")
+    elif data > 0:
+      print(str(data) + "positive")
+      
+            
+    
+username = ["juan", "pedro",  "james", "sagaad", "fuego"]
+
+ 
+for datausername in username:
+          print("james cute")
+          break;
+else:
+     print("james none")
+          
+              
